897b36fb9b6ab3dca21d03637b3eeda21e13ed1b
Giovanni Medeiros <ggabriel.medeiros@hotmail.com>
2024-03-23 21:37:34 -0300

    Inclusão de comentarios para melhor entendimento do código

--- script_qualys_report.py
+++ script_qualys_report.py
@@ -1,35 +1,37 @@
-import xml.etree.ElementTree as ET
-import csv
-from googletrans import Translator
+import xml.etree.ElementTree as ET  # Importa o módulo ElementTree para análise de XML
+import csv  # Importa o módulo csv para manipulação de arquivos CSV
+from googletrans import Translator  # Importa o tradutor do Google Translate
 
 def translate_text(texts):
     try:
-        translator = Translator()
+        translator = Translator()  # Inicializa o tradutor do Google Translate
+        # Traduz os textos para o português
         translated_texts = [translator.translate(text, src='auto', dest='pt').text for text in texts]
     except Exception as e:
-        print(f"Erro durante a tradução: {e}")
+        print(f"Erro durante a tradução: {e}")  # Exibe uma mensagem de erro se ocorrer algum problema durante a tradução
         translated_texts = texts  # Se ocorrer um erro, mantenha os textos originais
     return translated_texts
 
 def parse_xml(xml_file):
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
+    tree = ET.parse(xml_file)  # Faz o parse do arquivo XML
+    root = tree.getroot()  # Obtém o elemento raiz do XML
 
-    print("Script Iniciado")
+    print("Script Iniciado")  # Exibe uma mensagem indicando que o script foi iniciado
 
-    vulnerabilities = []
+    vulnerabilities = []  # Lista para armazenar as vulnerabilidades
 
     for ip_element in root.findall('.//IP'):
-        ip = ip_element.get('value')
+        ip = ip_element.get('value')  # Obtém o valor do atributo 'value' do elemento 'IP'
 
+        # Processa os elementos 'VULN' dentro de cada elemento 'IP'
         for vuln_element in ip_element.findall('.//VULN'):
-            severity = int(vuln_element.get('severity'))
-            threat = vuln_element.find('.//TITLE').text.strip()
-            impact = vuln_element.find('.//CONSEQUENCE').text.strip() if vuln_element.find('.//CONSEQUENCE') is not None else ''
-            solution = vuln_element.find('.//SOLUTION').text.strip() if vuln_element.find('.//SOLUTION') is not None else ''
+            severity = int(vuln_element.get('severity'))  # Obtém a gravidade da vulnerabilidade como um inteiro
+            threat = vuln_element.find('.//TITLE').text.strip()  # Obtém o título da vulnerabilidade
+            impact = vuln_element.find('.//CONSEQUENCE').text.strip() if vuln_element.find('.//CONSEQUENCE') is not None else ''  # Obtém o impacto da vulnerabilidade
+            solution = vuln_element.find('.//SOLUTION').text.strip() if vuln_element.find('.//SOLUTION') is not None else ''  # Obtém a solução para a vulnerabilidade
 
             if severity >= 2:
-                # Traduzindo os campos IMPACT e Solution
+                # Traduz os campos 'IMPACT' e 'Solution'
                 translated_impact, translated_solution = translate_text([impact, solution])
                 vulnerabilities.append({
                     'IP': ip,
@@ -39,13 +41,16 @@ def parse_xml(xml_file):
                     'Solution': translated_solution
                 })
 
+        # Processa os elementos 'CAT' dentro de cada elemento 'IP'
         for cat_element in ip_element.findall('.//CAT'):
-            cat_value = cat_element.get('value')
+            cat_value = cat_element.get('value')  # Obtém o valor do atributo 'value' do elemento 'CAT'
 
+            # Processa os elementos 'INFO' dentro de cada elemento 'CAT'
             for info_element in cat_element.findall('.//INFO'):
-                severity = int(info_element.get('severity'))
+                severity = int(info_element.get('severity'))  # Obtém a gravidade da vulnerabilidade como um inteiro
 
                 if severity >= 2:
+                    # Obtém e traduz a diagnóstico, consequência e solução da vulnerabilidade
                     diagnosis_element = info_element.find('.//DIAGNOSIS')
                     diagnosis = translate_text([diagnosis_element.text.strip()]) if diagnosis_element is not None else ''
 
@@ -64,10 +69,12 @@ def parse_xml(xml_file):
                         'Solution': solution[0] if solution else ''  # Ajuste para acessar o primeiro item da lista traduzida
                     })
 
+            # Processa os elementos 'SERVICE' dentro de cada elemento 'CAT'
             for service_element in cat_element.findall('.//SERVICE'):
-                severity = int(service_element.get('severity'))
+                severity = int(service_element.get('severity'))  # Obtém a gravidade da vulnerabilidade como um inteiro
 
                 if severity >= 2:
+                    # Obtém e traduz a diagnóstico, consequência e solução da vulnerabilidade
                     diagnosis_element = service_element.find('.//DIAGNOSIS')
                     diagnosis = translate_text([diagnosis_element.text.strip()]) if diagnosis_element is not None else ''
 
@@ -91,14 +98,14 @@ def parse_xml(xml_file):
 def write_to_csv(vulnerabilities, output_file):
     with open(output_file, mode='w', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=['IP', 'Category', 'Severity', 'THREAT', 'IMPACT', 'Solution'], delimiter=';')
-        writer.writeheader()
-        writer.writerows(vulnerabilities)
+        writer.writeheader()  # Escreve o cabeçalho no arquivo CSV
+        writer.writerows(vulnerabilities)  # Escreve as vulnerabilidades no arquivo CSV
 
 if __name__ == "__main__":
-    xml_file = "Hagana_CTH_0-60.xml"
-    output_file = "output_vulnerabilities.csv"
+    xml_file = "Hagana_CTH_0-60.xml"  # Nome do arquivo XML de entrada
+    output_file = "output_vulnerabilities.csv"  # Nome do arquivo CSV de saída
 
-    vulnerabilities = parse_xml(xml_file)
-    write_to_csv(vulnerabilities, output_file)
+    vulnerabilities = parse_xml(xml_file)  # Analisa o arquivo XML em busca de vulnerabilidades
+    write_to_csv(vulnerabilities, output_file)  # Escreve as vulnerabilidades traduzidas no arquivo CSV de saída
 
-print("Script Finalizado")
+print("Script Finalizado")  # Exibe uma mensagem indicando que o script foi finalizado
